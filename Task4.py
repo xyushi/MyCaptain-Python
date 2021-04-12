c556cdf5e1aec37e83d07977412c2e3c5d6ff956
Ayushi Poddar <74982763+xyushi@users.noreply.github.com>
2021-04-12 19:11:47 +0530

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,8 +1,10 @@
-s = input()
-counter = {}
-for char in s:
-    if char in counter:
-        counter[char] += 1 # counter[char]  = counter[char] + 1
-    else:
-        counter[char] = 1
-print(sorted(counter.items(), key=lambda x : x[1], reverse = True))
+def most_frequent():
+    s = input()
+    counter = {}
+    for char in s:
+        if char in counter:
+            counter[char] += 1 
+        else:
+            counter[char] = 1
+    print(sorted(counter.items(), key=lambda x : x[1], reverse = True))
+most_frequent()
