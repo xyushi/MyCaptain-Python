6f4becf8648a22914fd2358bf9c30e85182f3654
Ayushi Poddar <74982763+xyushi@users.noreply.github.com>
2021-04-12 13:33:30 +0530

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,8 @@
+s = input()
+counter = {}
+for char in s:
+    if char in counter:
+        counter[char] += 1 # counter[char]  = counter[char] + 1
+    else:
+        counter[char] = 1
+print(sorted(counter.items(), key=lambda x : x[1], reverse = True))
